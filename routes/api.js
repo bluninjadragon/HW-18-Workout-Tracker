242bd15d1f83aef5620939ade6d5abf1e2ee28bc
bluninjadragon <bluninjadragon@gmail.com>
2021-08-25 17:57:11 -0500

    updated api.js to include the aggregate function for the dashboard

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,21 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     });
 });
 
-//tutor said to use aggregates documentation from mongoose that he linked during our session
-router.get("/api/stats", ({ body }, res) => {});
+//tutor said to read the aggregates documentation from mongoose that he linked during our session
+router.get("/api/stats", (req, res) => {
+  Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: `exercises.duration` } } },
+  ])
+    //gets sort order of each path name DESCENDING (-1)
+    .sort({ _id: -1 })
+    //up to 7 path names
+    .limit(7)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
