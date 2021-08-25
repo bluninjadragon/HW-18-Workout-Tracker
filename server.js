3a809af8b218407776b78c0cfa03bd820e310b4e
bluninjadragon <bluninjadragon@gmail.com>
2021-08-25 12:26:57 -0500

    added a server to include api.js and views.js

---
+++ server.js
@@ -0,0 +1,31 @@
+//dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+//port
+const PORT = process.env.PORT || 3000;
+
+//expressapps
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  //use unify topology
+  useUnifiedTopology: true,
+});
+
+//routes
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/views.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
