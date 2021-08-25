fcc26a397d65cb2eec14a147735d11fa73d0a0da
bluninjadragon <bluninjadragon@gmail.com>
2021-08-25 17:32:13 -0500

    updated api to include get route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
     .then((dbWorkout) => {
