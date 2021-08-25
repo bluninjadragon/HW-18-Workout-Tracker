ad7f49a39ac6bd1abbde0743d4944e2a6b307cc6
bluninjadragon <bluninjadragon@gmail.com>
2021-08-25 11:47:11 -0500

    added model js file to create the schema and formatting for data that needs to be added

---
+++ Develop/model/tracker.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const trackerSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "What type of workout is this (Resistance or Cardio)?",
+  },
+  name: {
+    type: String,
+    required: "What is the name of this workoout?",
+  },
+  duration: {
+    type: Number,
+    required: "How long is this workout in minutes?",
+  },
+  weight: {
+    type: Number,
+    required: "How heavy are your weights? (Enter 0 if not using)",
+  },
+  reps: {
+    type: Number,
+    required: "How many reps?",
+  },
+  sets: {
+    type: Number,
+    required: "How many sets?",
+  },
+});
+
+const Tracker = mongoose.model("Tracker", trackerSchema);
+
+module.exports = Tracker;
